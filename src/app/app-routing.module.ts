98ce7cb4251d05568020662a2b65fed19c682550
LAURON <pauline.lauron@atos.net>
2020-05-26 12:17:02 +0200

    route part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: PageLoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
