d261a32a8aad2238a2ecc2dd062de35e1d3cd3f6
LAURON <pauline.lauron@atos.net>
2020-05-27 15:12:52 +0200

    theme dark

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,15 @@ import { TableLightComponent } from './components/table-light/table-light.compon
 import { BtnComponent } from './components/btn/btn.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective],
+  declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective, TableDarkComponent],
   imports: [
     CommonModule
   ],
-  exports: [TableLightComponent, BtnComponent, TotalPipe, StateDirective]
+  exports: [TableLightComponent, BtnComponent, TotalPipe, StateDirective, TableDarkComponent]
 })
 export class SharedModule { }
